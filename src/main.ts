e3250cb4b4cdb0cdb47d63fbf81e9bc199b9370c
entropy <146736857+Entropy420@users.noreply.github.com>
2025-02-28 00:27:10 +0530

    added trust proxy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
+  // Get the underlying Express instance
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', true); // Enable trust proxy
+
   app.enableCors({
     origin: 'https://abc.com',
   });
