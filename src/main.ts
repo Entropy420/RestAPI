883c04c3ef426487ff8da910a83a18436ce3b517
entropy <146736857+Entropy420@users.noreply.github.com>
2025-02-28 00:46:40 +0530

    added cors function

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,18 @@ async function bootstrap() {
   const expressApp = app.getHttpAdapter().getInstance();
   expressApp.set('trust proxy', true); // Enable trust proxy
 
+  // app.enableCors({
+  //   origin: 'https://abc.com',
+  // });
+
   app.enableCors({
-    origin: 'https://abc.com',
+    origin: (origin, callback) => {
+      if (origin === 'https://abc.com') {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
   });
 
   await app.listen(process.env.PORT ?? 3000);
