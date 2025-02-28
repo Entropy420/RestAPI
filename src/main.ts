47bcf87d642f6ffe12433bdbf8df51afec7d19eb
entropy <146736857+Entropy420@users.noreply.github.com>
2025-02-28 19:01:59 +0530

    Added a port obj in main.ts for render

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? 3000;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
   app.set('trust proxy', true);
 
   app.enableCors();
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
   console.log(`Application running on ${await app.getUrl()}`);
 }
 bootstrap();
